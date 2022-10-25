053dec17eeaef118845f37c5ffb0336bef1314fe
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2022-10-25 11:06:53 +0200

    Prepare 1.0.4 release
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Astarte.Device.MixProject do
   def project do
     [
       app: :astarte_device,
-      version: "1.0.3",
+      version: "1.0.4",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
