d0dde8d2f8d6226c8962e453a20174e2279a148f
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2023-05-25 14:59:50 +0200

    Bump Elixir and Erlang/OTP to 1.14 and 24
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Astarte.Device.MixProject do
     [
       app: :astarte_device,
       version: "1.1.0-dev",
-      elixir: "~> 1.11",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
