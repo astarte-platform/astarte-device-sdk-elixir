f9891bc1b6a1fd9ba881c3c2d8f9c65ff55248ef
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2023-09-25 17:14:43 +0200

    Update deps
    Just update astarte_core
    
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule Astarte.Device.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:astarte_core, "~> 1.0.4"},
+      {:astarte_core, "~> 1.0.5"},
       {:certifi, "~> 2.5"},
       {:hackney, "~> 1.15"},
       {:jason, "~> 1.1"},
